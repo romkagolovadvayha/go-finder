79ba50c9868b2fe6931ce81a8acd5a90c9081e3c
mescheryakov <mescheryakov@develop10.net.ilb.ru>
2020-01-24 11:03:01 +0400

    formatting

--- main.go
+++ main.go
@@ -8,20 +8,19 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+    scanner := bufio.NewScanner(os.Stdin)
 
     f := finder.NewFinder("Go")
     f.SetMaxCountWorkers(5)
 
     f.Render()
-	for scanner.Scan() {
+    for scanner.Scan() {
         f.Start(scanner.Text())
-	}
+    }
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalln(err)
-	}
+    if err := scanner.Err(); err != nil {
+        log.Fatalln(err)
+    }
 
     f.StopWait()
 }
-
